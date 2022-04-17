360da038d95f3437510a2d83f274974f604e5c42
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-04-17 15:18:22 +0300

    Completed timer function

--- src/js/foreground.js
+++ src/js/foreground.js
@@ -1,131 +1,64 @@
+//timer constant for timer
 const timerConstant = {
-    //type of minute
-    amountOfTime: 20,
-    //per of second
-    per: 60,
-    //timer function
-    timerInterval: null
+  //type of miliseconds
+  fullDash: 283,
+  timeLimit: 10,
+  timeLeft: 10,
+  //per of second
+  per: 1,
+  //timer function
+  timerInterval: null,
+};
+const colorLimits = {
+  danger: {
+    color: 'red',
+    limit: timerConstant.timeLimit / 4,
+  },
+  info: {
+    color: 'orange',
+    limit: timerConstant.timeLimit / 2,
+  },
+};
+//elems(html) of counts
+const elems = {
+  counterCircle: document.getElementById("counter-path-remaining"),
+  startButton: document.getElementById("start_button"),
+};
+//calculator stroke dash
+function calculateTimeFraction() {
+  const rawTimeFraction = timerConstant.timeLeft / timerConstant.timeLimit;
+  return (
+    rawTimeFraction - (1 / timerConstant.timeLimit) * (1 - rawTimeFraction)
+  );
 }
-
-function remainingTime() {
-    timerInterval = setInterval(() => {
-        console.log('foo');
-        timerConstant.amountOfTime--;
-    }, 1000)
-}
-document.addEventListener("DOMContentLoaded", () => {
-    remainingTime();
-})
-// const FULL_DASH_ARRAY = 283;
-// const WARNING_THRESHOLD = 10;
-// const ALERT_THRESHOLD = 5;
-
-// const COLOR_CODES = {
-//   info: {
-//     color: "green"
-//   },
-//   warning: {
-//     color: "orange",
-//     threshold: WARNING_THRESHOLD
-//   },
-//   alert: {
-//     color: "red",
-//     threshold: ALERT_THRESHOLD
-//   }
-// };
-
-// const TIME_LIMIT = 20;
-// let timePassed = 0;
-// let timeLeft = TIME_LIMIT;
-// let timerInterval = null;
-// let remainingPathColor = COLOR_CODES.info.color;
-
-// document.getElementById("counter").innerHTML = `
-// <div class="base-timer">
-//   <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-//     <g class="base-timer__circle">
-//       <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-//       <path
-//         id="base-timer-path-remaining"
-//         stroke-dasharray="283"
-//         class="base-timer__path-remaining ${remainingPathColor}"
-//         d="
-//           M 50, 50
-//           m -45, 0
-//           a 45,45 0 1,0 90,0
-//           a 45,45 0 1,0 -90,0
-//         "
-//       ></path>
-//     </g>
-//   </svg>
-//   <span id="base-timer-label" class="base-timer__label">${formatTime(
-//     timeLeft
-//   )}</span>
-// </div>
-// `;
-
-// startTimer();
-
-// function onTimesUp() {
-//   clearInterval(timerInterval);
-// }
-
-// function startTimer() {
-//   timerInterval = setInterval(() => {
-//     timePassed = timePassed += 1;
-//     timeLeft = TIME_LIMIT - timePassed;
-//     document.getElementById("base-timer-label").innerHTML = formatTime(
-//       timeLeft
-//     );
-//     setCircleDasharray();
-//     setRemainingPathColor(timeLeft);
-
-//     if (timeLeft === 0) {
-//       onTimesUp();
-//     }
-//   }, 1000);
-// }
-
-// function formatTime(time) {
-//   const minutes = Math.floor(time / 60);
-//   let seconds = time % 60;
-
-//   if (seconds < 10) {
-//     seconds = `0${seconds}`;
-//   }
-
-//   return `${minutes}:${seconds}`;
-// }
-
-// function setRemainingPathColor(timeLeft) {
-//   const { alert, warning, info } = COLOR_CODES;
-//   if (timeLeft <= alert.threshold) {
-//     document
-//       .getElementById("base-timer-path-remaining")
-//       .classList.remove(warning.color);
-//     document
-//       .getElementById("base-timer-path-remaining")
-//       .classList.add(alert.color);
-//   } else if (timeLeft <= warning.threshold) {
-//     document
-//       .getElementById("base-timer-path-remaining")
-//       .classList.remove(info.color);
-//     document
-//       .getElementById("base-timer-path-remaining")
-//       .classList.add(warning.color);
-//   }
-// }
-
-// function calculateTimeFraction() {
-//   const rawTimeFraction = timeLeft / TIME_LIMIT;
-//   return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
-// }
-
-// function setCircleDasharray() {
-//   const circleDasharray = `${(
-//     calculateTimeFraction() * FULL_DASH_ARRAY
-//   ).toFixed(0)} 283`;
-//   document
-//     .getElementById("base-timer-path-remaining")
-//     .setAttribute("stroke-dasharray", circleDasharray);
-// }
+//remaining time interval
+const updateTimer = () => {
+  updateCounterCircle();
+};
+//update counter circle width
+const updateCounterCircle = () => {
+  timerInterval = setInterval(() => {
+    timerConstant.timeLeft = timerConstant.timeLeft - timerConstant.per;
+    remainingTimeColor(timerConstant.timeLeft);
+    const circleDasharray = `${(
+      calculateTimeFraction() * timerConstant.fullDash
+    ).toFixed(0)} 283`;
+    elems.counterCircle.setAttribute("stroke-dasharray", circleDasharray);
+  }, 1000);
+};
+const startTimer = () => {
+  updateCounterCircle();
+};
+const remainingTimeColor = (timeLeft) => {
+  console.log(timeLeft, colorLimits.danger.limit);
+  if (colorLimits.danger.limit > timeLeft) {
+    elems.counterCircle.classList.add("red");
+  }
+  else if(colorLimits.info.limit > timeLeft){
+    elems.counterCircle.classList.add('orange');
+  }
+};
+//after page uploaded
+elems.startButton.addEventListener("click", () => {
+  startTimer();
+});
